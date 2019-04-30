7e0a04a9ce27b4c5293372f3213cded6615152fe
damarhr <50134980+damarhr@users.noreply.github.com>
2019-04-30 19:46:03 +0700

    Add files via upload

---
+++ technical-writter-test-master/SampleWebAPI/Controllers/PlayersController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebAPI.Models;
+
+namespace SampleWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        // GET api/players
+        [HttpGet]
+        public ActionResult<IEnumerable<Player>> Get()
+        {
+            Player[] players = null;
+            //Do query to db to get all records
+            return players;
+        }
+
+        // GET api/players/5
+        [HttpGet("{id}")]
+        public ActionResult<Player> Get(int id)
+        {
+            Player player = new Player();
+            //Query data to db, search Player with `id` = id and assign to player
+            return player;
+        }
+
+        // POST api/players
+        [HttpPost]
+        public void Post([FromBody] Player player)
+        {
+            //submit player data to database
+        }
+
+        // PUT api/players/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Player player)
+        {
+            //PUT player data with id to database
+        }
+
+        // DELETE api/players/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            //delete player data with `id` == id, if fail (not exist or something happen) return error
+        }
+    }
+}
---
+++ technical-writter-test-master/SampleWebAPI/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleWebAPI.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public int Score { get; set; }
+    }
+}
